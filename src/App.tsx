4a5be44f241be49b4d2054b39b2814c0d1c51a34
Matthew <12hughesm@gmail.com>
2023-12-12 15:38:00 +0000

    Implemenet basic searchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { TicketCard } from "./components/TicketCard/TicketCard";
 import team from "./data/team"
 import "./App.scss"
+import { SearchBar } from "./components/SearchBar/SearchBar";
 
 function App() {
   return (
     <main className="tracker-app">
       <h1 className="tracker-app__title">Ticket Tracker</h1>
+      <SearchBar/>
       <section className="tracker-cards">
         {team.map(member => (
           <TicketCard name={member.name} role={member.role}/>
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SearchBar = () => {
+  return (
+    <div>
+      <label htmlFor="name-input">Search By Name </label>
+      <input id="name-input" type="text" />
+    </div>
+  );
+};
