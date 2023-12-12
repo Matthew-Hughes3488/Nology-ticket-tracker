8c96c9b488c6b8c947cb0f6948672d0c4311ba7e
Matthew <12hughesm@gmail.com>
2023-12-12 12:44:47 +0000

    Created CounterProp type and added it to the HTML

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-export const Counter = () => {
+type CounterProps = {
+    handleIncrement: () => void,
+    handleDecrement: () => void
+}
+
+export const Counter = ({handleDecrement, handleIncrement} : CounterProps) => {
   return (
     <div className='counter'>
         <h2 className='counter__title'>Counter</h2>
         <p className='counter__tracker'>0</p>
-        <p className='counter__increment'>+</p>
-        <p className='counter__decrement'>-</p>
+        <p className='counter__increment' onClick={handleIncrement}>+</p>
+        <p className='counter__decrement' onClick={handleDecrement}>-</p>
     </div>
   )
 }
