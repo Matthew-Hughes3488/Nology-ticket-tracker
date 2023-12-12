32f078df642d6346dc13ff7813819df0ad412ca5
Matthew <12hughesm@gmail.com>
2023-12-12 12:33:12 +0000

    Basic tickcard HTML done

---
+++ src/components/TicketCard/TicketCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+export const TicketCard = () => {
+  return (
+    <div>
+        <h1>USER NAME HERE</h1>
+        <h1>ROLE HERE</h1>
+        /* ADD COUNTER COMPONENT HERE */
+    </div>
+  )
+}
