495d7f26d725b47c34633fa9c7ac823c091a7dfe
Matthew <12hughesm@gmail.com>
2023-12-12 15:02:24 +0000

    Added styles for table view aswell

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import { TicketCard } from "./components/TicketCard/TicketCard";
 import team from "./data/team"
+import "./App.scss"
 
 function App() {
   return (
-    <>
+    <main>
       <h1>Ticket Tracker</h1>
       <section className="tracker-cards">
         {team.map(member => (
           <TicketCard name={member.name} role={member.role}/>
         ))}
       </section>
-    </>
+    </main>
   );
 }
 
