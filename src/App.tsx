58496d704f6de42f911d2ea3fb12fb09a4d27217
Matthew <12hughesm@gmail.com>
2023-12-12 12:38:19 +0000

    Completed TicketCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { TicketCard } from "./components/TicketCard/TicketCard";
 
 function App() {
   return (
     <>
       <h1>Ticket Tracker</h1>
-      <section className="tracker-cards"></section>
+      <section className="tracker-cards">
+        <TicketCard name="Matthew" role="Big Boss Man"/>
+      </section>
     </>
   );
 }
