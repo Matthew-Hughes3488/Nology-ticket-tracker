5541b51d3aa71274c91d08009426fa53d8bfda1c
Matthew <12hughesm@gmail.com>
2023-12-12 13:02:19 +0000

    Used map to add ticketCards to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { TicketCard } from "./components/TicketCard/TicketCard";
+import team from "./data/team"
 
 function App() {
   return (
     <>
       <h1>Ticket Tracker</h1>
       <section className="tracker-cards">
-        <TicketCard name="Matthew" role="Big Boss Man"/>
+        {team.map(member => (
+          <TicketCard name={member.name} role={member.role}/>
+        ))}
       </section>
     </>
   );
