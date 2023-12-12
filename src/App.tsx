fadf41c34e7a80e297416ffb74af447b0c293cee
Matthew <12hughesm@gmail.com>
2023-12-12 16:59:06 +0000

    Fully implemented search by role

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,29 @@ import { DropDownBox } from "./components/DropDownBox/DropDownBox";
 
 function App() {
 
-  const [searchTerm, setSearchTerm] = useState<string>("")
+  const [searchTermName, setSearchTermName] = useState<string>("")
+  const [searchTermRole, setSearchTermRole] = useState<string>("")
 
 
   const handleInput = (event: FormEvent<HTMLInputElement>) =>{
-    setSearchTerm(event.currentTarget.value);
+    setSearchTermName(event.currentTarget.value);
+  }
+
+  const handleChange = (event: FormEvent<HTMLSelectElement>) =>{
+    setSearchTermRole(event.currentTarget.value);
   }
 
   const filteredTeam = team.filter(member =>{
-    return member.name.toLowerCase().includes(searchTerm.toLowerCase());
+    const namesMatch = member.name.toLowerCase().includes(searchTermName.toLowerCase());
+    if(searchTermRole === "") return namesMatch;
+    else return namesMatch && member.role.toLowerCase() === searchTermRole;
   })
 
   return (
     <main className="tracker-app">
       <h1 className="tracker-app__title">Ticket Tracker</h1>
       <SearchBar filterBy="Name" handleInput={handleInput}/>
-      <DropDownBox/>
+      <DropDownBox handleChange={handleChange}/>
       <section className="tracker-cards">
         {filteredTeam.map(member => (
           <TicketCard name={member.name} role={member.role}/>
--- src/components/DropDownBox/DropDownBox.tsx
+++ src/components/DropDownBox/DropDownBox.tsx
@@ -9,6 +9,7 @@ export const DropDownBox = ({handleChange}: DropDownBoxProps) => {
     <section className="dropdown-menu">
       <label htmlFor="employee-roles">Search By Role</label>
       <select name="role" id="employee-roles" onChange={handleChange}>
+      <option value="">All</option>
         <option value="junior software developer">
           Junior Software Developer
         </option>
@@ -17,7 +18,7 @@ export const DropDownBox = ({handleChange}: DropDownBoxProps) => {
           Senior Software Developer
         </option>
         <option value="tester">Tester</option>
-        <option value="Project Manager">Project Manager</option>
+        <option value="project manager">Project Manager</option>
         <option value="hard man">Hard Man</option>
       </select>
     </section>
